becbbf6c34b7dd69741d7c97f27cc5ee74a70c48
Horácio Pedro <46960527+horacio-pedro@users.noreply.github.com>
2020-06-02 13:34:42 +0100

    commit

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,14 @@ import express from 'express'
 
 const app = express()
 
+app.get('/users', (request, response) => {
+	console.log('Listagem de usuários')
+
+	response.json([
+		'Horácio',
+		'Miguel',
+		'Sucesso',
+	])
+})
+
 app.listen(2000)
